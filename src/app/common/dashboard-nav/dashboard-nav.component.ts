f0b0727788451c46408dbe92d1faf7e5a276f251
DESKTOP-HLB995O\immi <php.freak6@gmail.com>
2021-07-14 12:00:18 +0530

    fixed the design changes

--- src/app/common/dashboard-nav/dashboard-nav.component.ts
+++ src/app/common/dashboard-nav/dashboard-nav.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard-nav',
-  templateUrl: './dashboard-nav.component.html',
-  styleUrls: ['./dashboard-nav.component.css']
+	selector: 'app-dashboard-nav',
+	templateUrl: './dashboard-nav.component.html',
+	styleUrls: ['./dashboard-nav.component.css']
 })
 export class DashboardNavComponent implements OnInit {
 
-  constructor() { }
+	public openSideBarClass:string = '';
 
-  ngOnInit(): void {
-  }
+	constructor() { }
 
+	ngOnInit(): void {
+	}
+
+	openSideBar() {
+		console.log('inside openSideBar');
+		this.openSideBarClass = 'show-customer-sidebar';
+	}
+
+	closeSideBar() {
+		this.openSideBarClass = '';
+	}
 }
