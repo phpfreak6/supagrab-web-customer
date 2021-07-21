d3d5b4c11e3e4f065e0f1a279f8fb422563f862a
DESKTOP-HLB995O\immi <php.freak6@gmail.com>
2021-07-21 14:13:14 +0530

    implemented the FAQ list on customer panel

---
+++ src/app/services/faq.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { ConstantService } from './constant.service';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class FaqService {
+
+	public apiEndPoint: string;
+	public data: any;
+
+	constructor(
+		private httpClient: HttpClient,
+		private constantService: ConstantService
+	) {
+		this.apiEndPoint = this.constantService.apiBaseUrl;
+	}
+
+	getAllFaqs(): Observable<any> {
+
+		let url = `${this.apiEndPoint}/faqs`;
+		return this.httpClient
+			.get(url, this.constantService.getHttpJsonOptionsNoAuth())
+			.pipe(
+				map((e: any) => e),
+				catchError((e: Response) => throwError(e))
+		);
+	}
+}
