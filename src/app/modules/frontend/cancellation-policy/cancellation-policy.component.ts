80bf5b00234156a9e596edc3b284f2ae113bdefd
DESKTOP-HLB995O\immi <php.freak6@gmail.com>
2021-07-22 12:39:39 +0530

    implemented the api-1107 Get CMS by key cancellation policy page

--- src/app/modules/frontend/cancellation-policy/cancellation-policy.component.ts
+++ src/app/modules/frontend/cancellation-policy/cancellation-policy.component.ts
@@ -1,19 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 import { fadeInAnimation } from "../../../common/animations/fadein-animation";
 
+import { NgxSpinnerService } from "ngx-spinner";
+
+import { ConstantService } from "../../../services/constant.service";
+import { CmsService } from "../../../services/cms.service";
+
+import { PageInterface } from "../../../interfaces/PageInterface";
+
 @Component({
-  selector: 'app-cancellation-policy',
-  templateUrl: './cancellation-policy.component.html',
-  styleUrls: ['./cancellation-policy.component.css'],
-  animations: [
-    fadeInAnimation
-  ]
+	selector: 'app-cancellation-policy',
+	templateUrl: './cancellation-policy.component.html',
+	styleUrls: ['./cancellation-policy.component.css'],
+	animations: [
+		fadeInAnimation
+	]
 })
 export class CancellationPolicyComponent implements OnInit {
 
-  constructor() { }
+	isCmsKeyProvidedFlag = false;
+	cmsKey: any;
+	submitted = false;
+	cmsData: PageInterface;
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private ngxSpinnerService: NgxSpinnerService,
+		private cmsService: CmsService,
+		private constantService: ConstantService
+	) { }
+
+	ngOnInit(): void {
+		this.setCmsKey();
+	}
+
+	setCmsKey() {
+
+		this.getAddrById('cancellation-policy');
+	}
+
+	getAddrById(cmsKey) {
+
+		try {
+
+			this.ngxSpinnerService.show();
+			this.cmsService.getCmsByKey(cmsKey).subscribe(
+				(result) => {
+					if (result.success) {
+						this.cmsData = result.data.cms;
+					} else {
+						this.constantService.handleResCode(result);
+					}
+				},
+				(error) => {
+					this.ngxSpinnerService.hide();
+					console.log(error.message);
+					let obj = {
+						resCode: 400,
+						msg: error.message.toString(),
+					};
+					this.constantService.handleResCode(obj);
+				},
+				() => {
+					this.ngxSpinnerService.hide();
+				},
+			);
+
+		} catch (ex) {
+			this.ngxSpinnerService.hide();
+			let obj = {
+				resCode: 400,
+				msg: ex.toString(),
+			};
+			this.constantService.handleResCode(obj);
+		}
+	}
 
 }
