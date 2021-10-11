3fc098dc67cfbb7fb6cbc37a85e49cb4f283b6f0
phpfreak6 <php.freak6@gmail.com>
2021-10-11 16:00:57 +0530

    slideIn animation

--- src/app/common/animations/fadein-animation.ts
+++ src/app/common/animations/fadein-animation.ts
@@ -1,8 +1,27 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+// import { animate, state, style, transition, trigger } from '@angular/animations';
+
+// export let fadeInAnimation = trigger('fadeInAnimation', [
+//     transition('void => *', [
+//         style({ opacity: 0 }),
+//         animate(2000, style({ opacity: 1 }))
+//     ])
+// ])
+
+import { animate, style, transition, trigger } from '@angular/animations';
 
 export let fadeInAnimation = trigger('fadeInAnimation', [
     transition('void => *', [
-        style({ opacity: 0 }),
-        animate(2000, style({ opacity: 1 }))
-    ])
-])
+        style({
+            opacity: 0,
+            transform: 'translateX(150%)'
+        }),
+        animate(
+            // '450ms 400ms ease-out',
+            '450ms 400ms ease-out',
+            style({
+                transform: 'translateX(0%)',
+                opacity: 1
+            })
+        )
+    ]),
+]);
